baa1a5c6957e9ae2238937fa6aef937304cc1d43
Vedant Kotkar <vedantkotkar2607@gmail.com>
2026-07-23 21:16:43 +0530

    added ui in booking system

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -3,12 +3,21 @@ import { destinations } from "../data/destinations";
 import "../styles/Booking.css";
 import {Link} from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
+import { useState } from "react";
 
 
 function Booking(){
     const {id}  = useParams();
 
     const destination = destinations.find((item) => item.id === id);
+
+    const [travelers, setTravelers] = useState(1);
+
+    const price = parseInt(destination.price.replace(/[^\d]/g, "")
+    );
+
+    const total =
+    price * travelers;
 
     return(
         <div className="booking-page">
@@ -36,6 +45,22 @@ function Booking(){
                     <p className="booking-description">
                         {destination.description}
                     </p>
+                </div>
+
+                <div className="booking-features">
+
+                    <div className="features-card">
+                        Secure Booking
+                        <span> Your data is safe</span>
+                    </div>
+                    <div className="feature-card">
+                        Instant Confirmation
+                        <span>Quick Booking Process</span>
+                    </div>
+                    <div className="feature-card">
+                        24/7 Support
+                        <span>Always here to help</span>
+                    </div>
                 </div>
 
                 <div className="booking-right">
@@ -73,21 +98,54 @@ function Booking(){
                         <input
                         type="number"
                         min="1"
-                        defaultValue="1"
+                        value={travelers}
+                        onChange={(e) =>
+                            setTravelers(Number(e.target.value))
+                        }
                         />
 
+                    </div>
+
                     <div className="trip-info">
-                        <p> {destination.price}</p>
 
-                        <p> {destination.duration} </p>
+                        <div className="trip-card">
+                            <h4> Starting Price</h4>
+                            <p> {destination.price}</p>
+                        </div>
 
-                        <p>{destination.rating}</p>
+                        <div className="trip-card">
+                            <h4> Duration</h4>
+                            <p> {destination.duration} </p>
+                        </div>
 
-                        <p>{destination.bestTime}</p>
+                        <div className="trip-card">
+                            <h4> Rating</h4>
+                            <p>{destination.rating}</p>
+                        </div>
 
+                        <div className="trip-card">
+                            <h4>Best Time </h4>
+                            <p>{destination.bestTime}</p>
+                        </div>
 
                     </div>
+                        
+                    <div className="booking-summary">
+
+                        <div className="summary-row">
+                            <span>Trip Price</span>
+                            <span>{destination.price}</span>
                         </div>
+                        <div className="summary-row">
+                            <span> Travelers</span>
+                            <span>{travelers}</span>
+                        </div>
+                        <div className="summary-total">
+                            <span>Total Amount</span>
+                            <span> {total.toLocaleString()} RS/-</span>
+                        </div>
+
+                    </div>
 
                     <button type="submit" className="confirm-btn">
                         Confirm Booking
