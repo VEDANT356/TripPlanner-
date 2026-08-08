2a759ca9d8cde40c29899be8d85b8a5da622a387
Vedant Kotkar <vedantkotkar2607@gmail.com>
2026-08-08 19:35:26 +0530

    firebase fixed'

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { auth } from "../firebase/firebase";
 
-function ProtectedRoute({ children}) {
+function ProtectedRoute({ children }) {
     const user = auth.currentUser;
 
     if (!user) {
-        return <Navigate to="/login" replace />
+        return <Navigate to="/login" replace />;
     }
+
     return children;
 }
 
