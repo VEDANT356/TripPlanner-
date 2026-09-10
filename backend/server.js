fa25133db439a3864b7d0a6ca8180665a4db1a49
Vedant Kotkar <vedantkotkar2607@gmail.com>
2026-09-10 15:15:33 +0530

    make diffrent version

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ Content rules:
 
 async function callGemini(apiKey, systemInstruction, contents) {
     const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${apiKey}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}``,
         {
             method: "POST",
             headers: { "Content-Type": "application/json" },
